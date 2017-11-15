8e9abb40f7666b85d62c3299991e6745c8853dcd
Yann-Joël Dorsainville Tessier <yann@service-WIFI-EDUROAM-LA-14-107.nat.polymtl.ca>
2017-11-15 10:50:57 -0500

    first commit

---
+++ Automate.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Automate
+{
+    ArrayList adresses;
+
+    public void creerArbreAdresses(String fichier)
+    {
+        lireFichier(fichier);
+
+    }
+
+    private void lireFichier(String fichier)
+    {
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString() + "/autres/" + fichier;
+        Path path = Paths.get(s);
+
+        if(!Files.exists(path) || !Files.isRegularFile(path) || !Files.isReadable(path) || !Files.isExecutable(path)) {
+            System.out.println("Nom de fichier ou chemin invalide!\n");
+        } else {
+            try {
+                List<String> lines = Files.readAllLines(path);
+
+                for (String line : lines) {
+                    adresses.add(line);
+                }
+            } catch (IOException e) {}
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+            // Creation des objets principaux
+
+
+            // Choix du menu principal
+            int choixGeneral = 0;
+
+            while (choixGeneral == 0) // Lecture du choix principal
+            {
+                System.out.println(
+                                "MENU\n" +
+                                "(1) Créer l’automate.\n" +
+                                "(2) Traiter des requêtes.\n" +
+                                "(3) Afficher les statistiques.\n" +
+                                "(4) Quitter.\n");
+
+                Scanner scan1 = new Scanner(System.in);
+                choixGeneral = verifierEntreeUsager(scan1);
+
+                if (choixGeneral == 1) // Si l'usager choisi Drones
+                {
+                    System.out.println("Choix 1");
+
+                    Automate automate = new Automate();
+
+                    System.out.println("Nom du fichier : ");
+                    Scanner scan3 = new Scanner(System.in);
+                    String fichier = scan3.next();
+                    automate.creerArbreAdresses(fichier);
+
+                    choixGeneral=0;
+                }
+                else if (choixGeneral == 2) // Si l'usgager choisi Desserts
+                {
+                    System.out.println("Choix 2");
+                    choixGeneral=0;
+                }
+                else if (choixGeneral == 3) // Si l'usager choisi Quitter
+                {
+                    System.out.println("Choix 3");
+                    choixGeneral=0;
+                }
+                else if (choixGeneral == 4) return;
+                else if ((choixGeneral > 4 || choixGeneral < 1) && scan1.hasNextInt()  ) {
+                    choixGeneral = 0;
+                    System.out.println("Entrée invalide. Veuillez entrer une entrée valide");
+                }
+            }
+        }
+
+        public static int verifierEntreeUsager(Scanner scan){
+            if(scan.hasNextInt()) return scan.nextInt();
+            System.out.println("Entrée invalide. Veuillez entrer une entrée valide");
+            return 0;
+        }
+    }
