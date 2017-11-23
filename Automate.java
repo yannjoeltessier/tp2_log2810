d0e1e2d6350506a0e3145ffaa14a9e5ccd876af4
Yann-Joël Dorsainville Tessier <yann@Yann-Joels-MacBook-Pro.local>
2017-11-23 18:39:31 -0500

    minor changes

--- Automate.java
+++ Automate.java
@@ -1,3 +1,11 @@
+/**
+ * Description : Classe Automate implementant un automate permettant a validation d'adresses postale
+ * Auteurs     : Anoir Boujja, Yujia Ding & Yann-Joël D. Tessier
+ * Date        : 20 Novembre 2017
+ *
+ *
+ */
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -5,31 +13,118 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Automate
-{
-    ArrayList<String> codesPostaux = new ArrayList<String>();
+public class Automate {
+    static final int N_CHARACTERS_IN_CP = 6; // Le nb de caracteres dans un code postal (6)
 
-    public void creerArbreAdresses(String fichier)
-    {
-        lireFichier(fichier);
+    protected ArrayList<ArrayList<Character>> codesPostaux = new ArrayList<ArrayList<Character>>(N_CHARACTERS_IN_CP);
+    protected ArrayList<ArrayList<String>> requetes = new ArrayList<ArrayList<String>>();
+
+    /**
+     * Constructeur de l'automate
+     * */
+    public Automate() {
+        for (int i = 0; i < N_CHARACTERS_IN_CP; i++) {
+            ArrayList<Character> characters = new ArrayList<Character>();
+            codesPostaux.add(characters);
+        }
     }
 
-    private void lireFichier(String fichier)
+    /**
+     * Implementation d'un noeud
+     */
+    protected static class Node
     {
+        ArrayList<Character> val; // Valeur du noeud
+        Node right; // fils droit
+        Node left; // fils gauche
+
+        public Node(ArrayList<Character> val)
+        {
+            this.val = val;
+        }
+
+        private static void insert(Node node, ArrayList<Character> elem){
+            if (node == null) node = new Node(elem);
+            if (node.left != null){
+                insert(node.left, elem);
+            }
+            else node.left = new Node(elem);
+        }
+    }
+
+    protected Node root = null; // Racine de l'arbre
+
+    /**
+     * Lit le fichier des codes postaux et cree l'automate qui validera
+     * les requetes futures.
+     * @param fichier le nom du fichiers des codes postaux
+     * */
+    public void creerArbreAdresses(String fichier) {
+        lireFichier(fichier);
+        for (ArrayList<Character> ch : codesPostaux){
+            Node.insert(root, ch);
+        }
+    }
+
+    /**
+     * Permet de lire le fichier de codes postaux ou de requetes
+     * et remplie la liste (codePostaux ou requetes) correspondante.
+     * @param fichier un nom de fichier
+     * */
+    private void lireFichier(String fichier) {
         Path currentRelativePath = Paths.get("");
         String s = currentRelativePath.toAbsolutePath().toString() + "/src/autres/" + fichier;
         Path path = Paths.get(s);
 
-        if(!Files.exists(path) || !Files.isRegularFile(path) || !Files.isReadable(path) || !Files.isExecutable(path)) {
+        if (!Files.exists(path) || !Files.isRegularFile(path) || !Files.isReadable(path) || !Files.isExecutable(path)) {
             System.out.println("Nom de fichier ou chemin invalide!\n");
         } else {
             try {
                 List<String> lines = Files.readAllLines(path);
 
                 for (String line : lines) {
-                    codesPostaux.add(line);
+                    String[] split = line.split(" ");
+                    if (split.length > 1) {
+                        ArrayList<String> req = new ArrayList<String>();
+                        req.add(split[0]);
+                        req.add(split[1]);
+                        req.add(split[2]);
+                        requetes.add(req);
+                    } else {
+                        for (int i = 0; i < line.length(); i++) {
+                            if (!codesPostaux.get(i).contains(line.charAt(i)))
+                                codesPostaux.get(i).add(line.charAt(i));
+                        }
+                    }
                 }
-            } catch (IOException e) {}
+            } catch (IOException e) {
+            }
         }
+    }
+
+    /**
+     * Lit un fichier de requetes et les valide avant d'assigner les
+     * colis aux drones et d'envoyer ces drones vers d'autres stations.
+     * @param reqFile un fichier de requetes
+     * */
+    public void traiterLesRequetes(String reqFile) {
+        lireFichier(reqFile);
+    }
+
+    /**
+     * Permet d’assigner des colis à un drone en respectant les
+     * limitations physiques du drone et en s’assurant que tous
+     * les colis d’un drone vont vers le même quartier.
+     * */
+    public void assignerLesColis() {
+
+    }
+
+    /**
+     * Permet d’équilibrer le nombre de quadricoptères présents dans
+     * chaque quartier de la ville.
+     * */
+    public void equilibrerFlotte() {
+
     }
 }
--- Main.java
+++ Main.java
@@ -1,58 +1,74 @@
-import java.util.*;
+/**
+ * Description : Classe Main contenant la methode principale du programme
+ * Auteurs     : Anoir Boujja, Yujia Ding, Yann-Joël D. Tessier
+ * Date        : 20 Novembre 2017
+ *
+ *
+ * */
+
+import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
-            // Creation des objets principaux
+        // Creation des objets principaux
+        Automate automate = new Automate();
 
+        // Choix du menu principal
+        int choixGeneral = 0;
 
-            // Choix du menu principal
-            int choixGeneral = 0;
+        while (choixGeneral == 0) // Lecture du choix principal
+        {
+            System.out.println(
+                    "\nMENU\n" +
+                            "(1) Créer l’automate.\n" +
+                            "(2) Traiter des requêtes.\n" +
+                            "(3) Afficher les statistiques.\n" +
+                            "(4) Quitter.\n");
 
-            while (choixGeneral == 0) // Lecture du choix principal
+            Scanner scan1 = new Scanner(System.in);
+            choixGeneral = verifierEntreeUsager(scan1);
+
+            if (choixGeneral == 1) // Si l'usager choisi de creer l'automate
             {
-                System.out.println(
-                                "\nMENU\n" +
-                                "(1) Créer l’automate.\n" +
-                                "(2) Traiter des requêtes.\n" +
-                                "(3) Afficher les statistiques.\n" +
-                                "(4) Quitter.\n");
+                System.out.println("Choix #1");
 
-                Scanner scan1 = new Scanner(System.in);
-                choixGeneral = verifierEntreeUsager(scan1);
+                System.out.println("Nom du fichier des codes postaux : ");
+                Scanner scan3 = new Scanner(System.in);
+                String fichierCodesPostaux = "CodesPostaux.txt"; //scan3.next();
 
-                if (choixGeneral == 1) // Si l'usager choisi Drones
-                {
-                    System.out.println("Choix 1");
+                automate.creerArbreAdresses(fichierCodesPostaux);
 
-                    Automate automate = new Automate();
-
-                    System.out.println("Nom du fichier : ");
-                    Scanner scan3 = new Scanner(System.in);
-                    String fichier = scan3.next();
-                    automate.creerArbreAdresses(fichier);
-
-                    choixGeneral=0;
-                }
-                else if (choixGeneral == 2) // Si l'usgager choisi Desserts
-                {
-                    System.out.println("Choix 2");
-                    choixGeneral=0;
-                }
-                else if (choixGeneral == 3) // Si l'usager choisi Quitter
-                {
-                    System.out.println("Choix 3");
-                    choixGeneral=0;
-                }
-                else if (choixGeneral == 4) return;
+                choixGeneral = 0;
             }
-        }
+            else if (choixGeneral == 2) // Si l'usgager choisi de traiter les requetes
+            {
+                System.out.println("Choix #2");
 
-        public static int verifierEntreeUsager(Scanner scan){
-            if(scan.hasNextInt() && scan.nextInt() < 4 && scan.nextInt() > 1) return scan.nextInt();
-            System.out.println("Entrée invalide. Veuillez entrer une entrée valide");
-            return 0;
+                System.out.println("Nom du fichier des requêtes : ");
+                Scanner scan4 = new Scanner(System.in);
+                String fichierRequetes = "requetes1.txt"; //scan4.next();
+
+                automate.traiterLesRequetes(fichierRequetes);
+
+                choixGeneral = 0;
+            }
+            else if (choixGeneral == 3) // Si l'usager choisi d'afficher les statistiques
+            {
+                System.out.println("Choix #3");
+                choixGeneral = 0;
+            }
+            else if (choixGeneral == 4) return; // Si l'usager choisi Quitter
         }
     }
+
+    public static int verifierEntreeUsager(Scanner scan) {
+        if (scan.hasNextInt()){
+            int a = scan.nextInt();
+            if (a < 5 && a > 0) return a;
+        }
+        System.out.println("Entrée invalide. Veuillez entrer une entrée valide");
+        return 0;
+    }
+}
